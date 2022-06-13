98e36e3a6f982d7eba8e5cf325b86b0a990780d6
hayatshin <82467520+hayatshin@users.noreply.github.com>
2022-06-13 14:50:12 +0900

    expo-cli and preload

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/chungchun.png")];
+    const imagesPromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontsPromises, ...imagesPromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
